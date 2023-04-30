b2af81825bff24bb3bb5877ff0ebcf51c1a701de
Helder-Al <oindeterminavel@gmail.com>
2023-04-30 11:40:06 -0300

    adding project-files

---
+++ src/test/java/com/teste/cursed/CursedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.cursed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
