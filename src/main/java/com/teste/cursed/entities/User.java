706b4a34af86ba48f6e6812ce434f47c4e2e1d74
Helder-Al <oindeterminavel@gmail.com>
2023-04-30 12:33:14 -0300

    update on project

--- src/main/java/com/teste/cursed/entities/User.java
+++ src/main/java/com/teste/cursed/entities/User.java
@@ -1,4 +1,4 @@
-package entities;
+package com.teste.cursed.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
--- src/main/java/com/teste/cursed/resources/UserResources.java
+++ src/main/java/com/teste/cursed/resources/UserResources.java
@@ -1,7 +1,6 @@
-package resources;
+package com.teste.cursed.resources;
 
-import entities.User;
-import org.apache.catalina.connector.Response;
+import com.teste.cursed.entities.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
