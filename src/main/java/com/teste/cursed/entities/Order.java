8fccf6fef4f07025ac8d4c6c16b7e51471735e2d
Helder Alencar <oindeterminavel@gmail.com>
2023-06-11 14:10:28 -0300

    Descotinuação momentanea

--- src/main/java/com/teste/cursed/entities/Order.java
+++ src/main/java/com/teste/cursed/entities/Order.java
@@ -1,6 +1,7 @@
 package com.teste.cursed.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.teste.cursed.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,6 +20,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") //formatação da iso de horario
     private Instant moment;
 
+    private OrderStatus orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -28,9 +31,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        this.orderStatus = orderStatus;
         this.client = client;
     }
 
@@ -44,15 +48,22 @@ public class Order implements Serializable {
         return moment;
     }
 
+    public OrderStatus getOrderStatus(){
+        return orderStatus;
+    }
+
     public User getClient() {
         return client;
     }
 
+    //Setters
     public void setMoment(Instant moment) {
         this.moment = moment;
     }
 
-    //Setters
+    public void setOrderStatus(OrderStatus orderStatus){
+        this.orderStatus = orderStatus;
+    }
 
     public void setId(Long id) {
         this.id = id;
---
+++ src/main/java/com/teste/cursed/entities/enums/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.teste.cursed.entities.enums;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPING,
+    DELIVERED,
+    CANCELLED;
+}
